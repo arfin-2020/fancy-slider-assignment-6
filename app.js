db2bb2f15da5dc2d86ff90afa0a51e1af9f6071f
Arfin Chowdhury Arif <arfinchowdhuryarif7@gmail.com>
2021-02-14 22:02:40 +0600

    last commit

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ const sliderContainer = document.getElementById('sliders');
 
 // selected image 
 let sliders = [];
-
-
 // If this key doesn't work
 // Find the name in the url and go to their website
 // to create your own api key
-
 // here I am creating my own API
 
 const getImages = () => {
